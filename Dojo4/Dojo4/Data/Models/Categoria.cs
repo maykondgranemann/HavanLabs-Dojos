d2bdaad4a877849479d9373daff9274ae0cc089f
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-07-21 15:34:53 -0300

    criando base e categoria

---
+++ Dojo4/Dojo4/Data/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Categoria : BaseModel
+    {
+        public string Descricao { get; set; }
+        public string Nome { get; set; }
+    }
+}
