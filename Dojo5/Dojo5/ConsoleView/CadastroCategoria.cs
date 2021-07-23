d933b510d02a073d9bbcb01a7644132e1a5054b9
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-07-23 11:04:22 -0300

    Adicionado Console Cadastro Categoria

---
+++ Dojo5/Dojo5/ConsoleView/CadastroCategoria.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleView
+{
+    class CadastroCategoria
+    {
+
+        public void CadastrarCategoria()
+        {
+            Console.WriteLine("----------Bemvindo----------");
+            Console.WriteLine("Insira o ID");
+            string id = Console.ReadLine();
+            Console.WriteLine("Insira o Nome da Categoria");
+            string categoria = Console.ReadLine();
+            Console.WriteLine("Insira a descricao da Categoria");
+            string descricao = Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Dojo5/Dojo5/ConsoleView/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleView
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CadastroCategoria cadcategoria = new CadastroCategoria();
+            cadcategoria.CadastrarCategoria();
+            Console.ReadLine();
+        }
+    }
+}
