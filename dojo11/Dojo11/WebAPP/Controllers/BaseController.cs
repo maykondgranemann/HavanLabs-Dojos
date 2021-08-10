26fb199a54bcf483d8404474766e282331b6ed0f
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-08-10 17:36:19 -0300

    Tudo errado :(

---
+++ dojo11/Dojo11/WebAPP/Controllers/BaseController.cs
@@ -0,0 +1,52 @@
+﻿using Dojo11.Models;
+using Dojo11.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAPP.Controllers
+{
+    public class BaseController<M, R> : Controller where M: BaseModels where R: BaseRepository<M>
+     {
+        R repo = Activator.CreateInstance<R>();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(M model)
+        {
+            repo.Create(model);
+            return RedirectToAction("List");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View(repo.Read(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(M model)
+        {
+            repo.Update(model);
+            return RedirectToAction("List");
+        }
+        public ActionResult List()
+        {
+            return View(repo.Read());
+        }
+        public ActionResult Delete(int id)
+        {
+            repo.Delete(id);
+            return RedirectToAction("List");
+        }
+    }
+}
---
+++ dojo11/Dojo11/WebAPP/Controllers/FarmaciaController.cs
@@ -0,0 +1,15 @@
+﻿using Dojo11.Models;
+using Dojo11.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAPP.Controllers
+{
+    public class FarmaciaController : BaseController<Farmacia, FarmaciaRepository>
+    {
+ 
+    }
+}
