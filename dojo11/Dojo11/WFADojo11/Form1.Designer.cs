34b616a72f7e07ec0860faf9e4b3235bf840b8af
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-08-11 09:25:03 -0300

    WinForms

---
+++ dojo11/Dojo11/WFADojo11/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace WFADojo11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbNome = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txbBrisa = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txbPeso = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Nome";
+            // 
+            // txbNome
+            // 
+            this.txbNome.Location = new System.Drawing.Point(88, 78);
+            this.txbNome.Name = "txbNome";
+            this.txbNome.Size = new System.Drawing.Size(100, 20);
+            this.txbNome.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Brisa";
+            // 
+            // txbBrisa
+            // 
+            this.txbBrisa.Location = new System.Drawing.Point(88, 109);
+            this.txbBrisa.Name = "txbBrisa";
+            this.txbBrisa.Size = new System.Drawing.Size(100, 20);
+            this.txbBrisa.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Peso";
+            // 
+            // txbPeso
+            // 
+            this.txbPeso.Location = new System.Drawing.Point(88, 135);
+            this.txbPeso.Name = "txbPeso";
+            this.txbPeso.Size = new System.Drawing.Size(100, 20);
+            this.txbPeso.TabIndex = 1;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(88, 175);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 2;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(267, 254);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txbPeso);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txbNome);
+            this.Controls.Add(this.txbBrisa);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbNome;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txbBrisa;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txbPeso;
+        private System.Windows.Forms.Button btnSalvar;
+    }
+}
+
---
+++ dojo11/Dojo11/WFADojo11/Form1.cs
@@ -0,0 +1,35 @@
+﻿using Dojo11.Models;
+using Dojo11.Repository;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFADojo11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            DrogasRepository repo = new DrogasRepository();
+            Drogas model = new Drogas();
+            model.Nome = this.txbNome.Text;
+            model.Brisa = this.txbBrisa.Text;
+            model.Peso = Convert.ToDecimal(this.txbPeso.Text);
+            repo.Create(model);
+            this.txbNome.Text = String.Empty;
+            this.txbBrisa.Text = String.Empty;
+            this.txbPeso.Text = String.Empty;
+        }
+    }
+}
